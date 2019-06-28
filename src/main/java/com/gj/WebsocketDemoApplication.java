cc24fa22ec1d467a12156bc3ccd4340c2dc81db8
Gjing <me@gjing.cn>
2019-06-28 23:56:51 +0800

    实现群发消息

---
+++ src/main/java/com/gj/WebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gj/config/WebsocketConfiguration.java
@@ -0,0 +1,17 @@
+package com.gj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author Gjing
+ **/
+@Configuration
+public class WebsocketConfiguration {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
